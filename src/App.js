55cb367a0af3eec808543ade2495950e4961848f
“avinash8827” <avinashsinghbilawat279@gmail.com>
2022-02-07 13:17:47 +0530

    file uplodes

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import FileUpload from './Pages/FileUpload';
+import Error404 from './Pages/Error404';
+
+function App() {
+  return (
+      <Router>
+        <Routes>
+          <Route path="/" element={ <FileUpload /> } />
+          <Route path="/fileupload" element={ <FileUpload /> } />
+          <Route path="*" element={ <Error404 /> } />
+        </Routes>
+      </Router>
+  );
+}
+
+export default App;
